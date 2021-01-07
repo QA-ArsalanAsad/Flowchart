205922d63622369255b2b41491e1307b4c1ad2e8
Arsalan Asad <https://gitlab.com/arsalanasad96/python-pre-learning-tasks-published/-/tree/master>
2021-01-07 15:29:04 +0000

    flow

---
+++ src/com/qa/main/Flow.java
@@ -0,0 +1,16 @@
+package com.qa.main;
+
+public class Flow {
+	public void firstFlow(int a) {
+		do { 
+			System.out.println(a);
+			a++;
+		
+			
+		} while(a>200==false);
+		
+		
+		
+	}
+
+}
---
+++ src/com/qa/main/Runners.java
@@ -0,0 +1,10 @@
+package com.qa.main;
+
+public class Runners {
+	public static void main(String[] args) {
+		Flow fl = new Flow();
+		
+	 fl.firstFlow(100);
+		
+	}
+}
